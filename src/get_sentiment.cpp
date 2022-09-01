07bfd654455fce5b150d8d503498ae2f63526cdd
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2022-09-01 19:14:44 +0200

    work on get_sentiment fun

--- src/get_sentiment.cpp
+++ src/get_sentiment.cpp
@@ -2,14 +2,30 @@
 #include "algorithmic_search.h"
 #include "get_words_Cpp.h"
 #include "to_lowercase.h"
+#include "load_dict.h"
+#include "load_negation_words.h"
+#include "load_stopwords.h"
 
 using namespace Rcpp;
 
+// retrieve word lists
+const Rcpp::CharacterVector stopwords = load_stopwords();
+const Rcpp::CharacterVector negation_words = load_negation_words();
+
+
 //' @export
 // [[Rcpp::export]]
-CharacterVector get_sentiment(Rcpp::CharacterVector text) {
+Rcpp::CharacterVector get_sentiment(
+
+    Rcpp::CharacterVector text
+  ) {
+
+  // dict is too big to define out of function body.
+  // It will likely crash R if defined outside.
+  const Rcpp::DataFrame sentiment_dict = load_dict();
 
   CharacterVector words = to_lowercase(get_words_Cpp(text));
+
 
   return words;
 }
--- src/load_stopwords.h
+++ src/load_stopwords.h
@@ -1,6 +1,6 @@
 #include <Rcpp.h>
-#ifndef LOAD_DICT_H
-#define LOAD_DICT_H
+#ifndef LOAD_STOPWORDS_H
+#define LOAD_STOPWORDS_H
 
 // https://knausb.github.io/2017/08/header-files-in-rcpp/
 const Rcpp::CharacterVector load_stopwords();
